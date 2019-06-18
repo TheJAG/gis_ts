42acc19f1ae57ab1e2387d152f7e25c1cc0da765
michiel.jagersma <michiel.jagersma@veitchlister.com.au>
2019-06-18 21:53:45 +1000

    Playing around

---
+++ lib/tracks_bin_reader.py
@@ -0,0 +1,9 @@
+
+
+class TracksBinReader:
+    def __init__(self, tracks_bin):
+        self.tracks_bin = tracks_bin
+
+    def read_as_xml(self):
+        # TO DO serz it up!
+        return self.tracks_bin
