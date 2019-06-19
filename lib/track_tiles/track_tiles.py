4366eed70a402fb79113bc219bed4d828fdb9468
TheJAG <michiel.jagersma@veitchlister.com.au>
2019-06-19 22:30:00 +1000

    Might as well keep reading

--- lib/track_tiles/track_tiles.py
+++ lib/track_tiles/track_tiles.py
@@ -3,11 +3,15 @@ import os
 import struct
 import xml.etree.ElementTree as _et
 
+from math import atan2
+
 from lib.utils.bin_xml_converter import BinXmlConverter
 from lib.utils.utils import XMLUtils, MathUtils
 
 
 class TrackTiles:
+    TILE_SIZE_METRES = 1024.0
+
     def __init__(self, track_tiles_dir):
         self.track_tiles_dir = track_tiles_dir
 
@@ -38,9 +42,50 @@ class TrackTiles:
                     for curve in curves:
                         curve_type = curve.tag
                         curve_id = XMLUtils.ts_attrib(curve, "id")
-                        length_rounded = 3  # rounded to 3 decimals, work with alt_encoding instead
-                        length_precise = "alt_encoding equiv"
-                        # print(XMLUtils.ts_attrib(ribbon_and_curve, "id"))
+
+                        length_node = curve.find("Length")
+                        length_rounded = length_node.text
+                        length_precise = MathUtils.hex_to_double(XMLUtils.ts_attrib(length_node, "alt_encoding"))
+
+                        start_pos_node = curve.find("StartPos")
+                        start_pos_tile_find = "cFarVector2/{}/cFarCoordinate/RouteCoordinate/cRouteCoordinate/Distance"
+                        start_pos_tile_x = int(start_pos_node.find(start_pos_tile_find.format("X")).text)
+                        start_pos_tile_z = int(start_pos_node.find(start_pos_tile_find.format("Z")).text)
+
+                        start_pos_find = "cFarVector2/{}/cFarCoordinate/TileCoordinate/cTileCoordinate/Distance"
+                        start_pos_x = start_pos_node.find(start_pos_find.format("X"))
+                        start_pos_z = start_pos_node.find(start_pos_find.format("Z"))
+
+                        start_pos_x_rounded = float(start_pos_x.text)
+                        start_pos_x_precise = MathUtils.hex_to_double(XMLUtils.ts_attrib(start_pos_x, "alt_encoding"))
+                        start_pos_z_rounded = float(start_pos_z.text)
+                        start_pos_z_precise = MathUtils.hex_to_double(XMLUtils.ts_attrib(start_pos_z, "alt_encoding"))
+
+                        global_start_pos_x = start_pos_x_precise + TrackTiles.TILE_SIZE_METRES * start_pos_tile_x
+                        global_start_pos_z = start_pos_z_precise + TrackTiles.TILE_SIZE_METRES * start_pos_tile_z
+
+                        tan1, tan2 = map(float, curve.find("StartTangent").text.split(" "))
+                        angle = atan2(tan2, tan1)
+                        print(angle)
+
+                        print(
+                            " | ".join(
+                                map(
+                                    str,
+                                    [
+                                        curve_type,
+                                        length_precise,
+                                        start_pos_tile_x,
+                                        start_pos_tile_z,
+                                        start_pos_x_precise,
+                                        start_pos_z_precise,
+                                        angle,
+                                        global_start_pos_x,
+                                        global_start_pos_z,
+                                    ],
+                                )
+                            )
+                        )
 
     def read_all_track_tiles(self):
         return [
--- test/lib/track_tiles/track_tiles_test.py
+++ test/lib/track_tiles/track_tiles_test.py
@@ -5,7 +5,7 @@ from lib.track_tiles.track_tiles import TrackTiles
 
 class TracksTest(unittest.TestCase):
     def setUp(self):
-        track_tiles_dir = "e:\\Games\\Train Simulator NL\\steamapps\\common\\RailWorks\\Content\\Routes\\7977fcaa-b90f-4357-8e61-51aa2f24a13d\\Networks\\Track Tiles"
+        track_tiles_dir = "..\\..\\sample_data\\track_tiles_simple"
         self.track_tiles = TrackTiles(track_tiles_dir)
 
     def test_something(self):
